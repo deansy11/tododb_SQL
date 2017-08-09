77fc255fabddf9a173b8545edea027b11c0094cc
Emily Deans <emilyldeans@gmail.com>
2017-08-09 15:12:49 -0400

    Initial commit. Views, seeder, and routes files created.

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const models = require("../models/index");
+const todos = require("../models/todos");
 
-router.get('/todos', function(req, res) {
-  models.Todo.findAll()
+router.get('/', (req, res) => {
+  models.todos.findAll()
     .then((todos) => {
     res.render("index", {todos : todos});
   });
 });
 
-router.post('/todos', (req, res) => {
+router.post('/', (req, res) => {
+  // req.body = { button: '', toDoAdd: 'Walk the dog' }
   console.log(req.body);
-  models.Todo.build({
-    todoitem: req.body.title,
+  models.todos.create({
+    todoitem: req.body.toDoAdd,
     complete: false,
     createdAt: Date(),
     updatedAt: Date()
   })
-  .then(function(todo) {
-    res.json(todo);
-  });
+  res.render("index", { todos : req.body });
 });
+
+
+
+  // res.redirect('/');
+  // .then(function(todos) {
+    // res.render(req.body.title);
+
+  // models.todo.findById(req.params.id)
+  // res.render("index", { toDoList : req.body.title });
+  // router.get("/:id", (req, res) => {
+  //   models.todo.findById(req.params.id)
+  //   .then(todo => {
+  //     res.render("todos", { todo: todo });
+  //   });
+  // })
+
+// router.post('/:id', (req, res))
 
 module.exports = router;
---
+++ seeders/seed.js
@@ -0,0 +1,29 @@
+"use strict"
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert(
+      "todos",
+      [{
+          todoitem: 'Wake up.',
+          complete: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          todoitem: 'Eat breakfast',
+          complete: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }, {
+          todoitem: 'Get on the metro',
+          complete: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ], {});
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("todos", null, {});
+  }
+};
