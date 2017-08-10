51b5442079ce96b830fd08834eb137d4498f7b18
Emily Deans <emilyldeans@gmail.com>
2017-08-10 14:22:28 -0400

    Created successful 'post' for 'update' mustache.

--- routes/index.js
+++ routes/index.js
@@ -46,38 +46,19 @@ router.post("/:id/redo", (req, res) => {
 router.get("/:id/update", (req, res) => {
   console.log(req.params.id);
   models.todos.findById(req.params.id).then(todo => {
-      res.render("update", todo);
+    console.log(todo);
+      res.render("update", todo.dataValues);
   })
 })
 
 router.post("/:id/update", (req, res) => {
-  models.todos.findById(parseInt(req.params.id).then(todo => {
+  models.todos.findById(req.params.id).then(todo => {
+    console.log(req.params.id);
     todo.update({ todoitem : req.body.updateTodo }).then(todo => {
+      console.log(req.body.updateTodo);
       res.redirect("/")
     })
-  }))
+  })
 })
-
-
-// need to create a form (an additional view)
-// router.post("/:id/update", (req, res) => {
-//   models.todos.
-//
-// })
-
-  // res.redirect('/');
-  // .then(function(todos) {
-    // res.render(req.body.title);
-
-  // models.todo.findById(req.params.id)
-  // res.render("index", { toDoList : req.body.title });
-  // router.get("/:id", (req, res) => {
-  //   models.todo.findById(req.params.id)
-  //   .then(todo => {
-  //     res.render("todos", { todo: todo });
-  //   });
-  // })
-
-// router.post('/:id', (req, res))
 
 module.exports = router;
